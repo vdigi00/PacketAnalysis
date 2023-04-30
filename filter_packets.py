6e297156413491012f920e31fcf10af2a7c79929
jmm9341 <jmm9341@rit.edu>
2023-04-30 17:26:16 -0400

    Updated Reg's code
    Removed commented out sections to show conciseness

--- filter_packets.py
+++ filter_packets.py
@@ -1,4 +1,8 @@
-from scapy.all import *
+# Reg Chuhi, Vincent Digiovanni, Julian Moylan
+# NSSA 220 Project 2
+'''
+This file filters for ICMP data from raw txt files and outputs it to a filtered txt file
+'''
 
 def filter(node_num):
     cap_dir = "Captures/"
@@ -6,27 +10,6 @@ def filter(node_num):
 
     txt_filename = f'{cap_dir}Node{node_num}.txt'
     filtered_filename = f'{fil_dir}Node{node_num}_filtered.txt'
-
-    # packets = rdpcap(pcap_filename)
-
-    # filtered_packets = [p for p in packets if p.haslayer(ICMP) and p[ICMP].type in [8, 0]]
-    # print(filtered_packets)
-    # with open(filtered_filename, 'w') as f:
-    #     f.write('source_ip,dest_ip,type\n')  # Write header line
-    #     for p in filtered_packets:
-    #         src_ip = p[IP].src
-    #         dst_ip = p[IP].dst
-    #         icmp_type = 'request' if p[ICMP].type == 8 else 'reply'
-    #         f.write(f'{src_ip},{dst_ip},{icmp_type}\n')
-
-    # with open(txt_filename, 'w') as f:
-    #     f.write('source_ip,dest_ip,type\n')  # Write header line
-    #     for p in packets:
-    #         if p.haslayer(ICMP) and p[ICMP].type in [8, 0]:
-    #             src_ip = p[IP].src
-    #             dst_ip = p[IP].dst
-    #             icmp_type = 'request' if p[ICMP].type == 8 else 'reply'
-    #             f.write(f'{src_ip},{dst_ip},{icmp_type}\n')
     
     with open(txt_filename, 'r') as f, open(filtered_filename, 'w') as fw:
 		# Loop over every line in the file
@@ -37,6 +20,3 @@ def filter(node_num):
                 fw.write(line)
 
     return filtered_filename
-
-# for node_num in range(1, 5):
-#     filter(node_num)
