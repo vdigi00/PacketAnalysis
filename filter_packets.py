be7e3af571552fc224d981703c303138f6a1ee5d
jmm9341 <jmm9341@rit.edu>
2023-04-30 14:15:14 -0400

    Uploading latest from Reg
    Uploading for Reg again to avoid head merge conflicts

--- filter_packets.py
+++ filter_packets.py
@@ -1,34 +1,42 @@
 from scapy.all import *
 
 def filter(node_num):
-    icmp_filter = 'icmp and (icmp.type == 8 or icmp.type == 0)'
     cap_dir = "Captures/"
     fil_dir = "Filtered_Node_Files/"
 
-    pcap_filename = f'{cap_dir}Node{node_num}.pcap'
     txt_filename = f'{cap_dir}Node{node_num}.txt'
     filtered_filename = f'{fil_dir}Node{node_num}_filtered.txt'
 
-    packets = rdpcap(pcap_filename)
+    # packets = rdpcap(pcap_filename)
 
-    filtered_packets = [p for p in packets if p.haslayer(ICMP) and p[ICMP].type in [8, 0]]
+    # filtered_packets = [p for p in packets if p.haslayer(ICMP) and p[ICMP].type in [8, 0]]
+    # print(filtered_packets)
+    # with open(filtered_filename, 'w') as f:
+    #     f.write('source_ip,dest_ip,type\n')  # Write header line
+    #     for p in filtered_packets:
+    #         src_ip = p[IP].src
+    #         dst_ip = p[IP].dst
+    #         icmp_type = 'request' if p[ICMP].type == 8 else 'reply'
+    #         f.write(f'{src_ip},{dst_ip},{icmp_type}\n')
 
-    with open(filtered_filename, 'w') as f:
-        f.write('source_ip,dest_ip,type\n')  # Write header line
-        for p in filtered_packets:
-            src_ip = p[IP].src
-            dst_ip = p[IP].dst
-            icmp_type = 'request' if p[ICMP].type == 8 else 'reply'
-            f.write(f'{src_ip},{dst_ip},{icmp_type}\n')
+    # with open(txt_filename, 'w') as f:
+    #     f.write('source_ip,dest_ip,type\n')  # Write header line
+    #     for p in packets:
+    #         if p.haslayer(ICMP) and p[ICMP].type in [8, 0]:
+    #             src_ip = p[IP].src
+    #             dst_ip = p[IP].dst
+    #             icmp_type = 'request' if p[ICMP].type == 8 else 'reply'
+    #             f.write(f'{src_ip},{dst_ip},{icmp_type}\n')
+    
+    with open(txt_filename, 'r') as f, open(filtered_filename, 'w') as fw:
+		# Loop over every line in the file
+        for line in f:
+			# Check if it's an ICMP message
+            if("ICMP" in line):
+				# Write the line to the output file
+                fw.write(line)
 
-    with open(txt_filename, 'w') as f:
-        f.write('source_ip,dest_ip,type\n')  # Write header line
-        for p in packets:
-            if p.haslayer(ICMP) and p[ICMP].type in [8, 0]:
-                src_ip = p[IP].src
-                dst_ip = p[IP].dst
-                icmp_type = 'request' if p[ICMP].type == 8 else 'reply'
-                f.write(f'{src_ip},{dst_ip},{icmp_type}\n')
+    return filtered_filename
 
-for node_num in range(1, 5):
-    filter(node_num)
+# for node_num in range(1, 5):
+#     filter(node_num)
